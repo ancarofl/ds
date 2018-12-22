223408f40953c092d6ee84b39ac8d48114e2d39d
Anca N <negulescuanca11@gmail.com>
2018-12-22 16:44:50 +0100

    initial commit. stack.

---
+++ src/solution/Solution.java
@@ -0,0 +1,9 @@
+package solution;
+
+import structures.BasicStack;
+
+public class Solution {
+	public static void main(String[] args) {
+		System.out.println("Test");
+	}
+}
---
+++ src/structures/BasicStack.java
@@ -0,0 +1,43 @@
+package structures;
+
+//Java class "Stack" uses Vector in the underlying implementation
+//Java interface "Deque" is better
+public class BasicStack<X> {
+	private X[] data;
+	private int stackPointer;
+
+	//this is limited, replace array with ArrayList if max stack size unknown when instantiating
+	public BasicStack(int maxStackSize) {
+		data = (X[]) new Object[maxStackSize];
+		stackPointer = 0;
+	}
+
+	// insert on top
+	// O(1)
+	public void push(X newItem) {
+		data[stackPointer++] = newItem;
+	}
+
+	// delete/access top element
+	// O(1)
+	public X pop() {
+		if (stackPointer == 0) {
+			throw new IllegalStateException("Stack's empty.");
+		}
+		return data[--stackPointer];
+	}
+
+	// O(n)
+	public boolean contains(X item) {
+		for (int i = 0; i < stackPointer; i++) {
+			if (data[i].equals(item)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public int size() {
+		return stackPointer;
+	}
+}
